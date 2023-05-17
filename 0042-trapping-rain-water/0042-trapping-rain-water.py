c674a3b23f80ae49d1b526575f7f9e5dee346fc5
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-05-17 16:51:07 +0900

    Time: 127 ms (68.37%), Space: 18.4 MB (20.70%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-        
+        stack = []
         res = 0
-        left, right = 0, len(height) -1 # 투포인터
-        left_max, right_max = height[left], height[right]
-        
-        while left < right: # 역전 까지
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+        # 이중 반복문 구조
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 뺀다.
+                top = stack.pop()
+                
+                if not len(stack):
+                    break
+                    
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                res += distance * waters
             
-            # 더 높은 쪽을 향해 투 포인터 이동
-            if left_max <= right_max:
-                res += left_max - height[left]
-                left += 1
-            else:
-                res += right_max - height[right]
-                right -= 1
+            stack.append(i)
         
         return res
